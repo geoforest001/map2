c43f38e0537568c3f0b47d267b399c3ab5a8ebaf
geoforest001 <geoforest.001@gmail.com>
2026-07-28 17:32:15 +0900

    feat: maxZoom 25 with maxNativeZoom 18 for tile layers (v21)

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ const gsiAttribution =
   '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>';
 
 const map = L.map("map", {
-  zoomControl: true
+  zoomControl: true,
+  maxZoom: 25
 }).setView(fallbackLocation, fallbackZoom);
 
 const gsiStandard = L.tileLayer(
   "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
   {
     attribution: gsiAttribution,
-    maxZoom: 18,
+    maxNativeZoom: 18,
+    maxZoom: 25,
     className: "grayscale-layer bm-multiply"
   }
 );
@@ -21,7 +23,8 @@ const gsiAirPhoto = L.tileLayer(
   "https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
   {
     attribution: gsiAttribution,
-    maxZoom: 18,
+    maxNativeZoom: 18,
+    maxZoom: 25,
     className: "bm-multiply"
   }
 );
@@ -31,7 +34,8 @@ const naganoCsMap = L.tileLayer(
   {
     attribution:
       '<a href="https://www.geospatial.jp/ckan/dataset/nagano-csmap">長野県CS立体図</a>',
-    maxZoom: 18,
+    maxNativeZoom: 18,
+    maxZoom: 25,
     className: "bm-multiply"
   }
 );
